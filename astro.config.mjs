a6459b0262e7d511c156652817cd1e315a3595a4
KadzyLy <kadzyly@gmail.com>
2025-11-26 14:19:05 +0100

    chare: add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kadzyly.github.io',
   i18n: {
     locales: ["en", "ru"],
     defaultLocale: "en",
